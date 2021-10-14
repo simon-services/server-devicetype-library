535fd55a2c77768d2c27df4e576a901d24a36249
franciscsimon <francisc.simon@evalgo.org>
2021-10-14 13:40:04 +0200

    initial import

---
+++ create.py
@@ -0,0 +1,29 @@
+from os import write
+import yaml
+import ruamel.yaml
+
+manufacturer = "TestManufacturer"
+model = "test"
+slug = "test"
+
+device = {
+    "manufacturer": manufacturer,
+    "model": model,
+    "slug": slug,
+    "interfaces": [],
+    "console-ports": [],
+    "power-ports":[]
+}
+
+device['interfaces'].append({"name": "eth0","type": "1000base-t"})
+device['interfaces'].append({"name": "eth1","type": "1000base-t"})
+device['interfaces'].append({"name": "eth2","type": "1000base-t"})
+device['interfaces'].append({"name": "eth3","type": "1000base-t"})
+
+yaml_file = open(manufacturer+"/"+model+".yaml", "w")
+device_yaml = yaml.safe_dump(device, sort_keys=False, explicit_start=True)
+data = ruamel.yaml.round_trip_load(device_yaml)
+yaml_file.write( '---\n')
+for line in ruamel.yaml.round_trip_dump(data, indent=4, block_seq_indent=2).splitlines(True):
+    yaml_file.write(line)
+yaml_file.close()
